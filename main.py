4ffead59efb51c4e94bd476a0a515b666297aa40
Alex-Johnson-2 <65776036+AlexJohnson2@users.noreply.github.com>
2020-12-20 14:40:10 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 import stdiomask
 
-count=1
-while count<=3:
-    pass_ = stdiomask.getpass(prompt="\033[92m"+"Enter a Password (passwors esme to hast. peidash kon): ",mask="●")
-    if pass_=="mousavifard":
-        print("\nAffarin, password dorost bood \n")
-        break
-    else:
-        if count<3:
-            print("\npassword Eshtebahe,dobare emtehan kon \n")
-            count+=1
-        else:
-            break
-if count==3 and pass_!="mousavifard":
-    print("\nshoma bish az had talash kardid. lotfan badan talash konid")
-input("\nPress Enter to Exit ... \n")
-print("\033[0m"+"")
+class Password():
+    def __init__(self):
+        self.count = 1
+    def Enter_password(self,pass_):
+        while count<=3:
+            if pass_=="password":
+                print("\nAffarin, password dorost bood \n")
+                break
+            else:
+                if count<3:
+                    print("\npassword Eshtebahe,dobare emtehan kon \n")
+                    count+=1
+                else:
+                    break
+        if count==3 and pass_!="password":
+            print("\nshoma bish az had talash kardid. lotfan badan talash konid")
+        input("\nPress Enter to Exit ... \n")
+        print("\033[0m"+"")
+    def run_program(self):
+        self.Enter_password()
+if __name__=="__main__":
+    run = Password()
+    run.run_program()
+        
